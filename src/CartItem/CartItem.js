def6ecdc7ac1fa3f094d71a757ceda8c4ccd1583
AlonzoDEV06 <pe48qd@eduvaud.ch>
2025-11-25 13:12:52 +0100

    feat: test price_setPriceNominalCase_Succes done

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -44,6 +44,7 @@ module.exports = class CartItem {
 
     get price() {
         //TODO Implement this method
+        return this.#_price;
     }
 
     set price(value) {
@@ -54,6 +55,7 @@ module.exports = class CartItem {
 
     get total() {
         //TODO Implement this method
+        return this.#_quantity * this.#_price;
     }
     //endregion public methods
 
